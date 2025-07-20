de58b3b0870892e9ca0a12304408bf6f8bb643e6
Wang Handa <wanghanda1129@gmail.com>
2025-07-20 16:52:18 +0900

    调整画面layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,28 @@ import './App.css';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
-function App() {
+function AppRoutes() {
+  const location = useLocation();
+  // 只在首页显示 Header
+  const showHeader = location.pathname === '/';
 
   return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app-container">
+      {showHeader && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+}
